5ecc5a601e59f565b8c7b648446945e5c84cae1c
YangQing Li <2217175013@qq.com>
2023-11-17 16:17:47 +0000

    added routing for posting a new tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import { tasksData } from './tasksData';
-
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router(); 
 
@@ -9,7 +9,8 @@ router.get('/', (req, res) => {
     res.json(tasksData);
 });
 
-export default router;
+
+
 router.get('/:id', (req, res) => {
     const { id } = req.params
     const task = tasksData.tasks.find(task => task.id === id);
@@ -18,3 +19,19 @@ router.get('/:id', (req, res) => {
     }
     return res.status(200).json(task);
 });
+
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
+});
+export default router;
